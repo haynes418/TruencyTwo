a32e00ead550c1b9f2d4dd312ea9ae1f39bc5196
Logan <loganhyns@gmail.com>
2025-04-10 10:40:07 -0400

    Docker image file

--- react-proj/src/App.js
+++ react-proj/src/App.js
@@ -160,6 +160,7 @@ const App = () => {
             <Route path="/faq" element={<FAQPage />} />
             <Route path="/resources" element={<ResourcePage />} />
             <Route path="/chat" element={<DecisionTreeComponent node={decisionTree} />} />
+            <Route path="/fileupload" element={<FileUpload />} />
           </Routes>
         </main>
 
